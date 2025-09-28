99f3de315d90fc12120d91055bb3b6c930e541b5
Miu <m1ummap@gmail.com>
2025-09-28 11:36:56 -0300

    temporarily remove mtm support

--- inputbox.c
+++ inputbox.c
@@ -54,16 +54,6 @@ static inline void _input_remove_prev_word(inputbox_t *ib) {
 void input_update(inputbox_t *ib, int key) {
     switch (key) {
     case KEY_RESIZE: break;
-#ifdef _USE_MTM /* since i sometimes use the mtm terminal multiplexer */
-    case 200: {
-        switch (getch()) {
-        case 170: _input_prev_word(ib); break;
-        case 185: _input_next_word(ib); break;
-        case 144: _input_remove_next_word(ib); break;
-        default: break;
-        }
-    } break;
-#endif
     case KEY_LEFT:
         if (ib->pos > 0) --ib->pos;
         break;
